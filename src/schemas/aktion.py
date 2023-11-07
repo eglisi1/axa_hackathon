21af33d2a8d3b47c1d3d29ef111559719648f23d
Simon Egli <eglisi1@students.zhaw.ch>
2023-11-07 16:59:45 +0100

    create vectorization for law with metadata

--- src/schemas/aktion.py
+++ src/schemas/aktion.py
@@ -2,7 +2,12 @@ from schemadict import schemadict
 
 schema_aktion = schemadict(
     {
-        'id': {'type': int, 'required': True},
-        'beschreibung': {'type': str, 'required': True},
+        "id": {"type": int, "required": True},
+        "beschreibung": {"type": str, "required": True},
+        "artikel_id": {
+            "type": str,
+            "required": False,
+        },
+        "hat_artikel_verstossen": {"type": bool, "required": False},
     }
 )
--- src/schemas/sachverhalt.py
+++ src/schemas/sachverhalt.py
@@ -3,13 +3,13 @@ from aktion import schema_aktion
 
 schema_sachverhalt = schemadict(
     {
-        'beteiligter': {'type': str, 'required': True},
-        'fahrzeug': {'type': str, 'required': True},
-        'aktionen': {
-            'type': list,
-            'item_type': dict,
-            'required': True,
-            'item_schemadict': schema_aktion,
-        },
+        "beteiligter": {"type": str, "required": True},
+        "fahrzeug": {"type": str, "required": True},
+        "aktionen": {
+            "type": list,
+            "item_type": dict,
+            "required": True,
+            "item_schemadict": schema_aktion,
+        }
     }
 )
