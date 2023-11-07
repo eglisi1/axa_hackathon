9393cf01f6f4844093cf0f571bbb6c64b6067bd1
Simon Egli <eglisi1@students.zhaw.ch>
2023-11-07 22:00:28 +0100

    code improvements

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from util.logger import get_logger
 from util.config import load_config
@@ -16,6 +16,10 @@ def read_config() -> dict:
 
 
 @app.put("/predict")
-def read_item(request: Request) -> str:
-    logger.info(f"Received request: {request}")
-    return "prediction"
+def predict(request: Request) -> Request:
+    try:
+        logger.info(f"Received request: {request}")
+        return Request(text=request.text)
+    except Exception as e:
+        logger.error(f"Error during prediction: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error")
--- src/util/logger.py
+++ src/util/logger.py
@@ -2,8 +2,32 @@ import logging
 
 
 def get_logger(script_name: str, config: dict) -> logging.Logger:
-    logging.basicConfig(
-        level=config["logging"]["level"],
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    )
-    return logging.getLogger(script_name)
+    """
+    Creates and returns a logger with the given name and configuration.
+
+    Parameters:
+    script_name (str): The name of the script for which the logger is being created.
+    config (dict): The configuration dictionary with logging settings.
+
+    Returns:
+    logging.Logger: The configured logger.
+    """
+    logger = logging.getLogger(script_name)
+
+    # Set the logging level based on the config or default to WARNING.
+    log_level = config.get("logging", {}).get("level", "WARNING")
+    logger.setLevel(getattr(logging, log_level))
+
+    # Create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(getattr(logging, log_level))
+
+    # Create formatter and add it to the handlers
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    ch.setFormatter(formatter)
+
+    # Add the handlers to the logger
+    if not logger.handlers:
+        logger.addHandler(ch)
+
+    return logger
