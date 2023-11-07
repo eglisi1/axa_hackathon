248d65aae3c3810ca8ba0aef7c807f8e1e5b4ce8
rawside <pfeifsas@students.zhaw.ch>
2023-11-07 18:00:09 +0100

    optmized prompt

--- notebooks/01_situation_analysis/analyse_situation.py
+++ notebooks/01_situation_analysis/analyse_situation.py
@@ -11,25 +11,31 @@ from langchain.prompts import PromptTemplate
 openai.api_key  = os.environ['OPENAI_API_KEY']
  
 # Text-Input
-situation = "G kam über die Obere Zollgasse her gefharen, weiter in Richtung Ostermundigen. Auf Höhe der Verzweigung Waldheimstrasse kam gleichzeitig B über die Waldheimstrasse her gefahren und wollte nach rechts in die Obere Zollgasse einbiegen. B missachtete dabei den Vortritt und kollidierte so mit seiner linken Fahrzeugfront mit der rechten Fahrzeugseite von G."
+situation = "G1, G2 und B1 fuhren in genannter Reihenfolge mit ca. 50 km/h vom Beschleunigungsstreifen Bolligen herkommend auf dem Normalstreifen der A6-Süd R. B2 fuhr kurz vor dem Unfall vom Beschleunigungsstreifen Wankdorf herkommend mit ca. 50 km/h auf die A6-Süd R. G1 und G2 mussten, weil sie an ein Stauende heranfuhren, bis zum Stillstnand abbremsen. B1 bemerkte aufgrund fehlender Aufmerksamkeit das Stauende nicht und konnte in der Folge nicht rechtzeitig Abbremsen. Er kollidierte gegen das heck von G2 und schob G2 gegen das Heck von G1. B2 nahm den Unfall zu spät war und kollidierte gegen das Heck von B1. Im Nachgang wurde festgestellt, dass B2 über keinen gültigen Führerausweis verfügt und unter Drogeneinfluss stand."
  
 # Step 4: Define the Prompt Template
 prompt = PromptTemplate(
     input_variables=["concept"],
-    template= """You're a traffic specialist app, that gets information for a traffic accident. This is what happend {concept}, return only a python dictionary for every involved party with the strict following structure, seperate the objects with a |: 
-    element 1 = "beteiligter": "Sample Name",element 2 "fahrzeug": "Sample Vehicle", element 3"aktionen": as a list that contains max. 4 objects "id": 1, "beschreibung": "Sample Description,v max. 10 words per aktion""",
+    template= """You're a traffic specialist app, that gets information for a traffic accident. This is what happend {concept}, return only a python dictionary for every involved party with the strict following structure, seperate every involved party with a |: 
+    element 1 = "beteiligter": "Sample Name",element 2 "fahrzeug": "Vehicle", element 3"aktionen": as a list that contains max. 4 objects "id": 1, "beschreibung": "Sample Description,v max. 10 words per aktion""",
 )
  
 # Step 5: Print the Prompt Template
 print(prompt.format(concept=situation))
  
 # Step 6: Instantiate the LLMChain
-llm = OpenAI(temperature=0.2, model_name="gpt-3.5-turbo")
+llm = OpenAI(temperature=0.0, model_name="gpt-3.5-turbo")
 chain = LLMChain(llm=llm, prompt=prompt, verbose = True)
  
 # Step 7: Run the LLMChain
 output = chain.run(situation)
-print("Chain-Output" +" "+ output)
+print(output)
+
+if '|' in output:
+    print("Chain-Output: " + output)
+else:
+    # Wenn nicht, wird eine Exception geworfen
+    raise ValueError("Keine Trennung der Objekte durch | vorhanden")
 
 splitted_list = output.split('|')
 print("splittet_list") 
