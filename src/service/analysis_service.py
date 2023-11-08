aa3614ea8de89c1ba738a1cf875445727a5f6b2c
Simon Egli <eglisi1@students.zhaw.ch>
2023-11-08 16:34:58 +0100

    get less aktionen per party and call chain generation from util

--- src/service/analysis_service.py
+++ src/service/analysis_service.py
@@ -8,6 +8,7 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 
 from util.logger import get_logger
+from util.lang_chain_util import create_llm, create_llm_chain
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 if openai.api_key is None:
@@ -21,19 +22,14 @@ class AnalysisService:
         self.prompt_template = PromptTemplate(
             input_variables=["concept"],
             template="""You're a traffic specialist app, that gets information for a traffic accident. This is what happend {concept}, return only a python dictionary for every involved party with the strict following structure, seperate every involved party with a |: 
-            element 1 = "beteiligter": "Sample Name",element 2 "fahrzeug": "Vehicle", element 3"aktionen": as a list that contains max. 4 objects "id": 1, "beschreibung": "Sample Description,v max. 10 words per aktion""",
+            element 1 = "beteiligter": "Sample Name",element 2 "fahrzeug": "Vehicle", element 3"aktionen": as a list that contains max. 4 objects "id": 1, "beschreibung": "Sample Description,v max. 10 words per aktion and max 3 aktionen per involved party""",
         )
 
     def analyze_incident(self, situation_text: str) -> List[Dict]:
         self.logger.debug(f"Create OpenAI model with config: {self.config['situation_analysis']}")
-        llm = ChatOpenAI(
-            temperature=self.config["situation_analysis"]["temperature"],
-            model_name=self.config["situation_analysis"]["model_name"],
-        )
         self.logger.debug(f'Set concept to {situation_text}')
         self.prompt_template.format(concept=situation_text)
-        chain = LLMChain(llm=llm, prompt=self.prompt_template, verbose=self.config['situation_analysis']["verbose"])
-
+        chain = create_llm_chain(llm=create_llm, prompt=self.prompt_template)
         self.logger.debug(f'Run chain with situation: {situation_text}')
         output = chain.run(situation_text)
         self.logger.debug(f'Chain output: {output}')
