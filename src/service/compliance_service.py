e878a907ec2783ef2ea20ff0186bc890ab6f3ef8
Simon Egli <eglisi1@students.zhaw.ch>
2023-11-08 12:11:00 +0100

    remove unused services

--- src/service/compliance_service.py
+++
@@ -1,6 +0,0 @@
-from util.logger import get_logger
-
-class ComplianceService:
-    def __init__(self, config: dict):
-        self.config = config
-        self.logger = get_logger(__name__, config)
