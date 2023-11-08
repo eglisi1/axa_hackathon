8d9314bbeceec973fee3dd424a0bba5dcd5159f3
Nick Rascher <nick.rascher@hispeed.ch>
2023-11-08 09:13:56 +0100

    add svg apply prompt chain

--- notebooks/03_apply_svg/apply_svg.py
+++ notebooks/03_apply_svg/apply_svg.py
@@ -1,8 +1,11 @@
+import json
 import os
 import openai
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+from langchain.output_parsers import ResponseSchema
+from langchain.output_parsers import StructuredOutputParser
 
 openai.api_key = os.environ['OPENAI_API_KEY']
 
@@ -25,18 +28,53 @@ input = {
     ]
 }
 
+action_list = ''
+
+for aktion in input['aktionen']:
+    action_list += aktion['beschreibung'] + ', '
+
+violation_schema = ResponseSchema(name='violation',
+                                  description='Wurde gegen den Gesetzesartikel verstossen? \
+                             Antworte True wenn ja, oder antworte False wenn Nein oder es nicht klar ist.')
+
+reason_schema = ResponseSchema(name='reason',
+                               description='Begründe warum gegen den Gesetzesartikel \
+                             verstossen wurde oder warum nicht.')
+
+response_schemas = [violation_schema,
+                    reason_schema]
+
+violation_output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+
+format_instructions = violation_output_parser.get_format_instructions()
+
 violation_prompt_template = PromptTemplate(
-    input_variables=['aktion', 'artikel'],
-    template="Sag mir ob die Aktion '{aktion}' gegen den folgenden Gesetzesartikel '{artikel}' verstösst."
+    input_variables=['aktionsliste', 'artikel'],
+    template="""\
+        Evaluiere anhand der folgenden kommaseparierten Liste von Aktionen ob die Person gegen den folgenden Gesetzesartikel verstösst des folgenden  ob die folgende aktion gegen ihn verstösst und \
+        extrahiere die dazu folgenden Informationen: \
+        
+        Violation: Wurde gegen den Gesetzesartikel verstossen? Antworte True \
+        wenn ja, oder antworte False wenn Nein oder es nicht klar ist.gegen den folgenden Gesetzesartikel '{artikel}' verstösst.",
+        
+        Reason: Begründe warum gegen den Gesetzesartikel verstossen wurde oder warum nicht.
+    
+        Aktionsliste: {aktionsliste}
+        
+        Artikel: {artikel}
+        
+        Formattiere die Antwort in ein valides JSON.
+    """
 )
 
-for aktion in input["aktionen"]:
-    prompt = violation_prompt_template.format(aktion=aktion["beschreibung"], artikel=aktion['artikel_text'])
-
-    print(prompt)
+for aktion in input['aktionen']:
+    prompt = violation_prompt_template.format(aktionsliste=action_list, artikel=aktion['artikel_text'])
 
     llm = ChatOpenAI(temperature=0.0, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=violation_prompt_template, verbose=True)
 
-    output = chain.run({'aktion': aktion["beschreibung"], 'artikel': aktion['artikel_text']})
-    print("\nAntwort: " +" "+ output)
+    output = chain.run({'aktionsliste': action_list, 'artikel': aktion['artikel_text']}, )
+
+    output_dict = json.loads(output)
+
+    print(output_dict)
